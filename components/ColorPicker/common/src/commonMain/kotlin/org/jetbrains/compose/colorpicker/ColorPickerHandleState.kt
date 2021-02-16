33d904614dce56b9f1f877997f7f170c5df3ad3f
LAP2 <HrAJyW4@gmail.com>
2021-02-16 18:54:52 +0400

    add initial implementation for ColorPickerHandleState
    rename MovementBorder to MovementDeltaFilter

--- components/ColorPicker/common/src/commonMain/kotlin/org/jetbrains/compose/colorpicker/ColorPickerHandleState.kt
+++ components/ColorPicker/common/src/commonMain/kotlin/org/jetbrains/compose/colorpicker/ColorPickerHandleState.kt
@@ -1,3 +1,36 @@
 package org.jetbrains.compose.colorpicker
 
-interface MovementBorders
+import androidx.compose.foundation.MutatePriority
+import org.jetbrains.compose.movable.TwoDirectionsMovable
+import org.jetbrains.compose.movable.TwoDirectionsMoveScope
+import org.jetbrains.compose.movable.movableState
+
+interface MovementDeltaFilter {
+
+}
+
+class ColorPickerHandleState : TwoDirectionsMovable {
+
+    private val twoDirectionsMovableState = movableState(this::onMove)
+
+    private fun onMove(
+        xDelta: Float,
+        yDelta: Float
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun move(
+        movePriority: MutatePriority,
+        block: suspend TwoDirectionsMoveScope.() -> Unit) = twoDirectionsMovableState.move(movePriority, block)
+
+    override fun dispatchRawMovement(
+        xDelta: Float,
+        yDelta: Float
+    ) = twoDirectionsMovableState.dispatchRawMovement(xDelta, yDelta)
+
+    override val isMoveInProgress: Boolean = twoDirectionsMovableState.isMoveInProgress
+
+
+
+}
