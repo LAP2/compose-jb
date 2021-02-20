2a0de07e4e89a939acb3e81be11e550612928765
Danila Aliev <HrAJyW4@gmail.com>
2021-02-20 19:16:10 +0400

    split color picker on multiplatform parts.
    add initial color picker demo

--- components/ColorPicker/desktop/src/jvmMain/kotlin/org/jetbrains/compose/colorpicker/demo/Main.kt
+++ components/ColorPicker/desktop/src/jvmMain/kotlin/org/jetbrains/compose/colorpicker/demo/Main.kt
@@ -2,14 +2,17 @@ package org.jetbrains.compose.colorpicker.demo
 
 import androidx.compose.desktop.DesktopTheme
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.colorpicker.ColorPicker
 
 fun main() = Window(
-    "SplitPane demo"
+    "Color picker demo"
 ) {
     MaterialTheme {
         DesktopTheme {
-
+            ColorPicker(Modifier.fillMaxSize())
         }
     }
 }
